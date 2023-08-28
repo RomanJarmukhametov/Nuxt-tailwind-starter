3fe8a79b0b6323c19ffa45daec27278b6b71c161
Roman Jarmukhametov <108522445+RomanJarmukhametov@users.noreply.github.com>
2023-08-28 12:39:32 +0600

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,40 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+
+  ssr: true,
+  
+  css: ['@/assets/css/inter.css'],
+  
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'nuxt-headlessui',
+    '@nuxtjs/i18n',
+    '@nuxt/image',
+    'nuxt-schema-org',
+    'nuxt-simple-sitemap',
+    'nuxt-gtag',
+    '@vueuse/motion/nuxt'
+  ],
+
+   headlessui: {
+        prefix: 'Headless'
+  },
+   
+  i18n: {
+    locales: ['en', 'kk'],
+    defaultLocale: 'en',
+    strategy: 'prefix_except_default',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      alwaysRedirect: true,
+    },
+  },
+
+  gtag: {
+    // use actual ID here
+    id: 'G-XXXXXXXXXX' 
+  }
+
+})
